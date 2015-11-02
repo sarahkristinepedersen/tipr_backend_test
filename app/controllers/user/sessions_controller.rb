77a1ba16e2bee7ea2f17a04c4483b94655626f8d
Aaron <aarongkendall@gmail.com>
2015-11-02 15:19:54 +0000

    devise backend set up

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,25 +1,15 @@
 class User::SessionsController < Devise::SessionsController
-# before_filter :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  respond_to :json, :html
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
-  # end
+  def create
+    sign_out(:user) if current_user
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_flashing_format?
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+      respond_with resource, location
+      after_sign_in_path_for(resource)
+    end
+    
 end
